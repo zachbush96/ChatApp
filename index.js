9c526790942fe98b7915ae139901720f6960b585
zachbush96 <81973485+zachbush96@users.noreply.github.com>
2021-04-21 17:27:22 -0500

    Update index.js
    created server vairable with express .listen() and gave that to socket.io instead of app()

--- index.js
+++ index.js
@@ -3,15 +3,22 @@ const express = require('express')
 const app = express();
 const PORT = process.env.PORT || 8080;
 
-//Import socketIO and setup CORS to get/send messages to and from any domain
-const io = require('socket.io')(app, {
-  cors: { origin: "*" }
-});
 
 //Serve static HTML file
 app.use(express.static("landing-page"));
 app.get('/', (req,res) => {
   res.sendFile('index.html', {root:__dirname + '/landing-page'});
+});
+
+
+const server = app.listen(PORT, "Listening on port " + PORT);
+//http.listen(process.env.PORT || 8080, () => console.log('Listening'));
+
+
+
+//Import socketIO and setup CORS to get/send messages to and from any domain
+const io = require('socket.io')(server, {
+  cors: { origin: "*" }
 });
 
 
@@ -22,8 +29,3 @@ io.on('connection', (socket) => {
     io.emit('message', '${socket.id.substr(0,2)} said ${message}' );  
   });
 });
-
-
-
-app.listen(PORT, "Listening on port " + PORT);
-//http.listen(process.env.PORT || 8080, () => console.log('Listening'));
