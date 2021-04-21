c222c437683a874deaabf637ec868ab26fae07a3
zachbush96 <81973485+zachbush96@users.noreply.github.com>
2021-04-21 17:22:13 -0500

    Update index.js
    http --> express FW

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
-const http = require('http').createServer();
-const io = require('socket.io')(http, {
+//const http = require('http').createServer();
+const express = require('express')
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+//Import socketIO and setup CORS to get/send messages to and from any domain
+const io = require('socket.io')(app, {
   cors: { origin: "*" }
 });
 
+//Serve static HTML file
+app.use(express.static("landing-page"));
+app.get('/', (req,res) => {
+  res.sendFile('index.html', {root:__dirname + '/landing-page'});
+});
 
 
 io.on('connection', (socket) => {
@@ -13,4 +23,7 @@ io.on('connection', (socket) => {
   });
 });
 
-http.listen(process.env.PORT || 8080, () => console.log('Listening'));
+
+
+app.listen(PORT, "Listening on port " + PORT);
+//http.listen(process.env.PORT || 8080, () => console.log('Listening'));
