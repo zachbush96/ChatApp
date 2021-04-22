54a717068402b569c92919941850d9ec492eccb8
zachbush96 <81973485+zachbush96@users.noreply.github.com>
2021-04-21 19:39:32 -0500

    Update index.js
    console log last 10

--- index.js
+++ index.js
@@ -22,6 +22,7 @@ io.on('connection', (socket) => {
   }
   socket.on('message', (msg) => {
     last10.push(`${socket.id.substr(0,2)} said ${msg}`);
+    console.log("Last 10: "+last10);
     console.log("message: " + msg);
     io.emit('message', `${socket.id.substr(0,2)} said ${msg}` );  
   });
